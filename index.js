0e5e38b4a5685fbf6816fd89b6090b2ba8ee6153
saadyousafi87 <saad.yousafi87@gmail.com>
2022-11-06 13:09:33 -0500

    added code to init function and writeToFile function

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
-const generateMarkdown = require('./utils/generateMarkdown.js');
+const helper = require("./utils/generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
 const questions = ["Enter the Project Title", "Enter the Project Description" 
 , "Enter the Table of Contents separating with space","Enter the Installation Instruction"
 , "Enter the Usage Information", "Enter the Credits Instruction"
 , "Choose a license", "Enter the Contribution Guidelines"
-, "Enter the Tests Instruction"];
+, "Enter the Tests Instruction", "Enter GitHub username"
+, "Enter your email address"];
 
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    if(fs.existsSync("./"+fileName)){
+        fs.appendFile(fileName, helper.generateMarkdown(data), (err) =>
+        err ? console.error(err) : console.log('File writing successful!'));
+    }else {
+        fs.writeFile(fileName, helper.generateMarkdown(data), (err) =>
+        err ? console.error(err) : console.log('File writing successful!'));
+    }
+}
 
 // TODO: Create a function to initialize app
 function init() {
@@ -50,7 +59,7 @@ function init() {
         {
             type: 'checkbox',
             message: questions[6],
-            choices: ["None", "MIT License", "Apache License 2.0", "GNU General Public License v3.0"],
+            choices: ["None", "MIT", "Apache 2", "GPL"],
             name: 'license',
         },
         {
@@ -62,10 +71,20 @@ function init() {
             type: 'input',
             message: questions[8],
             name: 'testInfo',
+        },
+        {
+            type: 'input',
+            message: questions[9],
+            name: 'username',
+        },
+        {
+            type: 'input',
+            message: questions[10],
+            name: 'email',
         }
     ])
     .then((response) => {
-        console.log({response});
+        writeToFile("README.md", response);
     });
 }
 
